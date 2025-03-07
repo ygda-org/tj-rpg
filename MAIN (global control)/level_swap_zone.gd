2eb33c8f56b5472f6b1f25fd0b795dffa478fb64
sroshc <shirzaysroshsafa@gmail.com>
2025-03-07 15:21:42 -0500

    Added a test main scene and fixed problem with saving main scene

--- MAIN (global control)/level_swap_zone.gd
+++ MAIN (global control)/level_swap_zone.gd
@@ -3,9 +3,17 @@ extends Area2D
 # always add this as a direct child of the area scene
 
 @export var to_scene_path: String = "res://" # path of the level we're moving to through here
+@export var one_shot: bool = false
+@export var save_scene: bool = false
 
 func switch_scene() -> void: # the most wonderful few lines of code you'll ever witness
-	SceneSwitcher.save_scene_and_goto(to_scene_path)
+	if(save_scene):
+		SceneSwitcher.save_scene_and_goto(to_scene_path)
+	else:
+		SceneSwitcher.goto_scene(to_scene_path)
+	
+	if(one_shot):
+		queue_free()
 
 func _on_body_entered(body):
 	if (body.name == "Player"):
--- MAIN (global control)/scene_switcher.gd
+++ MAIN (global control)/scene_switcher.gd
@@ -23,21 +23,30 @@ func _deferred_goto_scene(path):
 	
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
-	
-	
+
 func save_scene_and_goto(path) -> Variant:
+	_deferred_save_scene_and_goto.call_deferred(path)
+	
+	return saved_scene_result
+	
+	
+func _deferred_save_scene_and_goto(path):
 	current_scene.process_mode = Node.PROCESS_MODE_DISABLED
 	current_scene.hide()
-	
+	get_tree().root.remove_child(current_scene)
+
 	saved_scene = current_scene
 	
 	var scene_loader = ResourceLoader.load(path)
 	current_scene = scene_loader.instantiate()
 	get_tree().root.add_child(current_scene)
 	
-	resolver = get_tree().create_timer(0)
-	await resolver.timeout
 	
+	resolver = get_tree().create_timer(9999) #Temporary await (make better later)
+	await resolver.timeout
+	resolver = null
+	
+	print("timed out!!")
 	current_scene.queue_free()
 	current_scene = saved_scene
 	saved_scene = null
@@ -45,9 +54,9 @@ func save_scene_and_goto(path) -> Variant:
 	current_scene.process_mode = Node.PROCESS_MODE_INHERIT
 	current_scene.show()
 	
+	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
 	
-	return saved_scene_result
 
 
 func end_temp_scene(result: Variant):
