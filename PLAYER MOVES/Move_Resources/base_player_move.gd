4adde1328bfe59747ad51e27c8262b3c3f61698f
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-05-09 21:28:12 -0400

    Started created more area stuff, added special battle stats, redid some sizing, and more misc stuff
    blablabla it was multiple unrelated things

--- PLAYER MOVES/Move_Resources/base_player_move.gd
+++ PLAYER MOVES/Move_Resources/base_player_move.gd
@@ -5,3 +5,6 @@ class_name BasePlayerMove
 @export var damage: float = 0.0
 @export var accuracy: float = 1.00
 @export var crit_chance: float = 0.05
+@export var player_speed: int = 2 
+@export var player_defense: float = 1.0 
+@export var player_size_scale: float = 1.0 
